996c1ecadc0614364e49eeb719f5ae4574f23c1b
RenanGamerProOficial <69207411+RenanGamerProOficial@users.noreply.github.com>
2022-11-04 15:11:59 -0300

    Update OptionsState.hx

--- source/options/OptionsState.hx
+++ source/options/OptionsState.hx
@@ -29,13 +29,18 @@ using StringTools;
 
 class OptionsState extends MusicBeatState
 {
-	var options:Array<String> = ['Note Colors', 'Controls', 'Adjust Delay and Combo', 'Graphics', 'Visuals and UI', 'Gameplay'];
+	var options:Array<String> = ['Note Colors', 'GameJolt', 'Controls', 'Adjust Delay and Combo', 'Graphics', 'Visuals and UI', 'Gameplay'];
 	private var grpOptions:FlxTypedGroup<Alphabet>;
 	private static var curSelected:Int = 0;
 	public static var menuBG:FlxSprite;
 
 	function openSelectedSubstate(label:String) {
 		switch(label) {
+                        case 'GameJolt':
+				#if android
+				removeVirtualPad();
+				#end
+				openSubState(new options.GameJolt());
 			case 'Note Colors':
 				#if android
 				removeVirtualPad();
